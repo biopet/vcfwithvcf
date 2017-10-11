323a261e5ec19cf1af12cbc789e253ee9fdd79d8
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2017-10-11 09:50:37 +0200

    object to class

--- src/test/scala/nl/biopet/tools/vcfwithvcf/VcfWithVcfTest.scala
+++ src/test/scala/nl/biopet/tools/vcfwithvcf/VcfWithVcfTest.scala
@@ -3,7 +3,7 @@ package nl.biopet.tools.vcfwithvcf
 import nl.biopet.test.BiopetTest
 import org.testng.annotations.Test
 
-object VcfWithVcfTest extends BiopetTest {
+class VcfWithVcfTest extends BiopetTest {
   @Test
   def testNoArgs(): Unit = {
     intercept[IllegalArgumentException] {
